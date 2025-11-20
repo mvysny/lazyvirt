e4cf85a8aafd48504aa1cd4dbb4f45d65b6f1a83
Martin Vysny <mavi@vaadin.com>
2025-11-20 12:07:40 +0200

    VirtCache tests: port to rspec

---
+++ spec/virtcache_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require_relative 'spec_helper'
+require 'virtcache'
+require 'vm_emulator'
+require 'timecop'
+
+describe VirtCache do
+  it 'smokes' do
+    VirtCache.new(VMEmulator.new)
+  end
+
+  context 'total_vm_rss_usage' do
+    it 'is 0 for no VMs' do
+      assert_equal 0, VirtCache.new(VMEmulator.new).total_vm_rss_usage
+    end
+
+    it 'is calculated properly' do
+      Timecop.freeze(Time.now) do
+        assert_equal 2_415_919_104, VirtCache.new(vm_emulator_demo).total_vm_rss_usage
+      end
+    end
+  end
+
+  context 'running?' do
+    it 'works on demo data' do
+      c = VirtCache.new(vm_emulator_demo)
+      assert c.running?('Ubuntu')
+      assert c.running?('win11')
+      assert !c.running?('BASE')
+      assert !c.running?('non-existing-cm')
+    end
+  end
+end
